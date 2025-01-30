a14739e3c93e2b672fd845e4e15afae42bdef049
Roelof Roos <github@roelofr.dev>
2025-01-31 00:36:46 +0100

    Add Tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
+import vueDevtools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer()]
+    }
+  },
+  plugins: [
+    vue(),
+    vueDevtools()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
