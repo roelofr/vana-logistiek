acc06d761d0c6623920455ea0f6857175e10598c
Roelof Roos <github@roelofr.dev>
2025-02-03 13:33:44 +0100

    fix: styling;

--- frontend/src/components/app/AppServiceWorker.vue
+++ frontend/src/components/app/AppServiceWorker.vue
@@ -3,25 +3,19 @@ import { useRegisterSW } from 'virtual:pwa-register/vue'
 import { toast } from 'vue-sonner'
 import { watch } from 'vue'
 
-const {
-    offlineReady,
-    needRefresh,
-    updateServiceWorker
-} = useRegisterSW()
+const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW()
 
 watch(offlineReady, (value) => {
-    if (!value)
-        return
+    if (!value) return
 
     toast('Applicatie gedownload', {
         description: 'De applicatie is gedownload, dus werkt nu ook met minder goede verbindingen.',
-        duration: 10_000
+        duration: 10_000,
     })
 })
 
 watch(needRefresh, (value) => {
-    if (!value)
-        return
+    if (!value) return
 
     toast('Nieuw versie beschikbaar', {
         dismissible: true,
@@ -30,6 +24,6 @@ watch(needRefresh, (value) => {
             label: 'Herladen',
             onClick: updateServiceWorker,
         },
-    });
+    })
 })
 </script>
