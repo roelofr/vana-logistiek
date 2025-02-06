f780a1e9856b683e046621eb124d2efc9f34f6e8
Roelof Roos <github@roelofr.dev>
2025-02-06 19:51:38 +0100

    feat: favicon

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     plugins: [
         vue(),
         vueDevtools({
-            launchEditor: "idea",
+            launchEditor: 'idea'
         }),
         vitePWA({
             strategies: 'injectManifest',
@@ -35,13 +35,37 @@ export default defineConfig({
             },
 
             // Generate manifest
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
             manifest: {
                 name: 'LogistiekApp',
                 description: 'De MegaSuperApp voor Logistiek',
                 background_color: '#020817',
                 theme_color: '#0d4298',
                 display: 'minimal-ui',
-                orientation: 'natural'
+                orientation: 'natural',
+                icons: [
+                    {
+                        src: 'pwa-64x64.png',
+                        sizes: '64x64',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'maskable-icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    }
+                ]
             }
         })
     ],
