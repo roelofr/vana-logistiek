b05f181f800ff9e31fa7ac7e68e8a453554c45a8
Roelof Roos <github@roelofr.dev>
2025-02-20 21:57:12 +0100

    feat: added saving

--- frontend/src/views/CreateTicketView.vue
+++ frontend/src/views/CreateTicketView.vue
@@ -7,6 +7,13 @@ import { ticketTypesMap, type Vendor } from '@/domain'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Heading, Paragraph } from '@/components/ui/typography'
 import PickTitle from '@/views/CreateTicketView/PickTItle.vue'
+import { Button } from '@/components/ui/button'
+import { Check } from 'lucide-vue-next'
+import { confetti } from '@/lib'
+import { useRouter } from 'vue-router'
+import { toast } from 'vue-sonner'
+
+const router = useRouter()
 
 const currentStep = ref(1)
 const vendor = ref<null | Vendor>(null)
@@ -17,6 +24,25 @@ const currentType = computed(() => {
 
     return null
 })
+
+const save = () => {
+    if (!vendor.value || !type.value || !desc.value) {
+        toast.error('Er missen nog velden!')
+        return
+    }
+
+    const toastDesc = `Je hebt een ${currentType.value?.label} aangemaakt voor ${vendor.value.name} (#${vendor.value.id}) met een omschrijving.`
+
+    toast.success('Ticket created!', {
+        description: toastDesc,
+        dismissible: true,
+        duration: 0,
+    })
+
+    confetti()
+
+    router.push({ path: '/' })
+}
 </script>
 <template>
     <AppContainer content size="narrow">
@@ -31,32 +57,42 @@ const currentType = computed(() => {
             <PickVendor v-if="currentStep == 1" v-model="vendor" @submit="currentStep = 2" />
             <PickType v-else-if="currentStep == 2" v-model="type" @submit="currentStep = 3" />
             <PickTitle v-else-if="currentStep == 3" v-model="desc" @submit="currentStep = 4" />
-            <Card v-else>
-                <CardHeader>
-                    <CardTitle>That's all folks</CardTitle>
-                    <CardDescription>De rest is nog niet af :(</CardDescription>
-                </CardHeader>
 
-                <CardContent>
-                    <dl class="grid grid-cols-1 gap-4 md:grid-cols-[250px_1fr]">
-                        <dt class="font-bold">Standhouder</dt>
-                        <dd class="text-muted-foreground">
-                            {{ vendor!.name }} ({{ vendor!.number }})
-                        </dd>
+            <template v-else>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>That's all folks</CardTitle>
+                        <CardDescription>De rest is nog niet af :(</CardDescription>
+                    </CardHeader>
 
-                        <dt class="font-bold">Type</dt>
-                        <dd class="text-muted-foreground flex items-center">
-                            <Component :is="currentType?.icon" class="h-4 w-4 mr-2" />
-                            {{ currentType!.label }}
-                        </dd>
+                    <CardContent>
+                        <dl class="grid grid-cols-1 gap-4 md:grid-cols-[250px_1fr]">
+                            <dt class="font-bold">Standhouder</dt>
+                            <dd class="text-muted-foreground">
+                                {{ vendor!.name }} ({{ vendor!.number }})
+                            </dd>
 
-                        <dt class="font-bold">Omschrijving</dt>
-                        <dd class="text-muted-foreground flex items-center">
-                            {{ desc }}
-                        </dd>
-                    </dl>
-                </CardContent>
-            </Card>
+                            <dt class="font-bold">Type</dt>
+                            <dd class="text-muted-foreground flex items-center">
+                                <Component :is="currentType?.icon" class="h-4 w-4 mr-2" />
+                                {{ currentType!.label }}
+                            </dd>
+
+                            <dt class="font-bold">Omschrijving</dt>
+                            <dd class="text-muted-foreground flex items-center">
+                                {{ desc }}
+                            </dd>
+                        </dl>
+                    </CardContent>
+                </Card>
+
+                <div class="text-right">
+                    <Button variant="default" @click.prevent="save" class="flex items-center gap-2">
+                        <Check class="h-4" />
+                        Opslaan
+                    </Button>
+                </div>
+            </template>
         </div>
     </AppContainer>
 </template>
