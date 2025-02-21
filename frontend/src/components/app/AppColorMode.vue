395ff677a6b11f04d6f2545af8c16768ecbaf061
Roelof Roos <github@roelofr.dev>
2025-02-21 22:26:12 +0100

    fix: fix theme toggle styling

--- frontend/src/components/app/AppColorMode.vue
+++ frontend/src/components/app/AppColorMode.vue
@@ -11,13 +11,15 @@ const toggle = () => {
 </script>
 
 <template>
-    <Button variant="outline" @click="toggle" class="flex items-center justify-start gap-x-2">
-        <div class="relative">
-            <Sun class="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <MoonStar
-                class="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-            />
-        </div>
+    <Button
+        variant="outline"
+        @click="toggle"
+        class="w-full flex items-center justify-start gap-x-2"
+    >
+        <Sun class="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <MoonStar
+            class="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        />
         <span class="font-bold">Wissel thema</span>
     </Button>
 </template>
