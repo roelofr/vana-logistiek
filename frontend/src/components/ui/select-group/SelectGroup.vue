0c445a26d52c14e9530d3970b965d90fda6f67c1
Roelof Roos <github@roelofr.dev>
2025-02-22 10:52:59 +0100

    refactor: remove unused components

--- frontend/src/components/ui/select-group/SelectGroup.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-const { label } = defineProps<{ label: string }>()
-</script>
-
-<template>
-    <fieldset :aria-label="label" class="-space-y-px rounded-md bg-background">
-        <slot />
-    </fieldset>
-</template>
