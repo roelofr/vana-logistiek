f93a6a52446929d661587cf76d10c488fe827962
Roelof Roos <github@roelofr.dev>
2025-04-25 23:36:25 +0200

    fix: re-add linting

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,12 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'frontend'
 }
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,16 +1,14 @@
-import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {routes} from './app.routes';
-import {ClarityModule} from "@clr/angular";
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core'
+import { provideRouter } from '@angular/router'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+import { routes } from './app.routes'
+import { ClarityModule } from '@clr/angular'
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({eventCoalescing: true}),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    importProvidersFrom(
-      ClarityModule
-    ),
-  ]
-};
+    importProvidersFrom(ClarityModule),
+  ],
+}
