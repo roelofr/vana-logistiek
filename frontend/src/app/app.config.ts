3537152960ac3a77387cecc0fc8a2d71326fc5fb
Roelof Roos <github@roelofr.dev>
2025-05-08 22:15:05 +0200

    feat: ssr

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,16 +5,18 @@ import {routes} from './app.routes'
 import {TemplatePageTitleStrategy} from './global/template-page-title-strategy.service';
 import {provideAppIcons} from './app.icons';
 import {provideHttpClient, withFetch} from '@angular/common/http';
+import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
+    provideClientHydration(withEventReplay()),
     provideRouter(routes),
     provideHttpClient(
       withFetch()
     ),
     provideAnimationsAsync(),
     {provide: TitleStrategy, useClass: TemplatePageTitleStrategy},
-    provideAppIcons()
+    provideAppIcons(),
   ],
 }
