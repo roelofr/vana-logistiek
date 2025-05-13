5793861282e1babe772775c89a03b8ab0d0b178d
Roelof Roos <github@roelofr.dev>
2025-05-13 20:29:03 +0200

    fix: test

--- frontend/src/app/views/login/login.component.spec.ts
+++ frontend/src/app/views/login/login.component.spec.ts
@@ -3,6 +3,8 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {LoginComponent} from './login.component';
 import {provideExperimentalZonelessChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {provideHttpClientTesting} from '@angular/common/http/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -12,6 +14,8 @@ describe('LoginComponent', () => {
     await TestBed.configureTestingModule({
       providers: [
         provideExperimentalZonelessChangeDetection(),
+        provideHttpClient(withFetch()),
+        provideHttpClientTesting(),
         provideRouter([{path: '**', component: LoginComponent}]),
       ],
       imports: [LoginComponent],
