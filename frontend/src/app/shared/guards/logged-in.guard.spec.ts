44ecc89b90484c0c4e15d7258f7b596261e7e3ab
Roelof Roos <github@roelofr.dev>
2025-06-17 23:31:01 +0200

    wip: make stuff

--- frontend/src/app/shared/guards/logged-in.guard.spec.ts
+++ frontend/src/app/shared/guards/logged-in.guard.spec.ts
@@ -2,7 +2,7 @@ import {TestBed} from '@angular/core/testing';
 
 import {LoggedInGuard} from './logged-in.guard';
 import {AuthService} from '../../services/global/auth.service';
-import {ActivatedRouteSnapshot, Router, RouterStateSnapshot} from "@angular/router";
+import {Router} from "@angular/router";
 import {provideZonelessChangeDetection} from "@angular/core";
 
 describe('LoggedInGuard', () => {
@@ -10,9 +10,6 @@ describe('LoggedInGuard', () => {
 
   const authServiceSpy = jasmine.createSpyObj('AuthService', ['isLoggedIn']);
   const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
-
-  const activatedRoute = {} as ActivatedRouteSnapshot;
-  const routerState = {url: '/login'} as RouterStateSnapshot
 
   beforeEach(async () => {
     TestBed.configureTestingModule({
@@ -34,7 +31,7 @@ describe('LoggedInGuard', () => {
   it('should work when logged in', async () => {
     authServiceSpy.isLoggedIn.and.returnValue(true);
 
-    const result = guard.canActivate(activatedRoute, routerState)
+    const result = guard.canActivate()
     expect(result).toEqual(true);
 
     expect(authServiceSpy.isLoggedIn.calls.count())
@@ -43,14 +40,14 @@ describe('LoggedInGuard', () => {
     expect(routerSpy.navigate.calls.count())
       .toBe(0);
 
-    const resultChild = guard.canActivateChild(activatedRoute, routerState)
+    const resultChild = guard.canActivateChild()
     expect(resultChild).toEqual(true);
   });
 
   it('should return a redirect when not logged in', async () => {
     authServiceSpy.isLoggedIn.and.returnValue(false);
 
-    const result = guard.canActivate(activatedRoute, routerState)
+    const result = guard.canActivate()
     expect(result).toEqual(false);
 
     expect(authServiceSpy.isLoggedIn.calls.count())
@@ -62,7 +59,7 @@ describe('LoggedInGuard', () => {
     expect(routerSpy.navigate.calls[0].args)
       .toBe(['/login']);
 
-    const resultChild = guard.canActivateChild(activatedRoute, routerState)
+    const resultChild = guard.canActivateChild()
     expect(resultChild).toEqual(false);
   });
 });
--- frontend/src/app/shared/guards/not-logged-in.guard.spec.ts
+++ frontend/src/app/shared/guards/not-logged-in.guard.spec.ts
@@ -2,16 +2,17 @@ import {TestBed} from '@angular/core/testing';
 
 import {NotLoggedInGuard} from './not-logged-in.guard';
 import {AuthService} from '../../services/global/auth.service';
-import {ActivatedRouteSnapshot, Router, RouterStateSnapshot} from "@angular/router";
-import {provideZonelessChangeDetection} from "@angular/core";
+import {Router} from "@angular/router";
+import {provideZonelessChangeDetection, signal} from "@angular/core";
 
 describe('NotLoggedInGuard', () => {
     let guard: NotLoggedInGuard;
-    const authServiceSpy = jasmine.createSpyObj('AuthService', ['isLoggedIn']);
-    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
-
-    const activatedRoute = {} as ActivatedRouteSnapshot;
-    const routerState = {url: '/login'} as RouterStateSnapshot
+    const authServiceSpy = {
+        isLoggedIn: jest.fn(signal(false)),
+    };
+    const routerSpy = {
+        navigate: jest.fn(() => Promise.resolve()),
+    };
 
     beforeEach(async () => {
         TestBed.configureTestingModule({
@@ -31,37 +32,37 @@ describe('NotLoggedInGuard', () => {
     });
 
     it('should return a redirect when logged in', async () => {
-        authServiceSpy.isLoggedIn.and.returnValue(true);
+        authServiceSpy.isLoggedIn.mockReturnValue(true);
 
-        const result = guard.canActivate(activatedRoute, routerState)
+        const result = guard.canActivate()
         expect(result).toEqual(false);
 
-        expect(authServiceSpy.isLoggedIn.calls.count())
+        expect(authServiceSpy.isLoggedIn.mock.calls.length)
             .toBe(1);
 
-        expect(routerSpy.navigate.calls.count())
+        expect(routerSpy.navigate.mock.calls.length)
             .toBe(1);
 
-        expect(routerSpy.navigate.calls[0].args)
+        expect(routerSpy.navigate.mock.calls[0])
             .toBe(['/login']);
 
-        const resultChild = guard.canActivateChild(activatedRoute, routerState)
+        const resultChild = guard.canActivateChild()
         expect(resultChild).toEqual(false);
     });
 
     it('should no-op when not logged in', async () => {
-        authServiceSpy.isLoggedIn.and.returnValue(false);
+        authServiceSpy.isLoggedIn.mockReturnValue(false);
 
-        const result = guard.canActivate(activatedRoute, routerState)
+        const result = guard.canActivate()
         expect(result).toEqual(true);
 
-        expect(authServiceSpy.isLoggedIn.calls.count())
+        expect(authServiceSpy.isLoggedIn.mock.calls.length)
             .toBe(1);
 
-        expect(routerSpy.navigate.calls.count())
+        expect(routerSpy.navigate.mock.calls.length)
             .toBe(0);
 
-        const resultChild = guard.canActivateChild(activatedRoute, routerState)
+        const resultChild = guard.canActivateChild()
         expect(resultChild).toEqual(true);
     });
 });
