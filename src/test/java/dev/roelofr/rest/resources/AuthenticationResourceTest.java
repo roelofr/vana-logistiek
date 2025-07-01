d63a399f494501e734ebb5ef05dc21e8a1173668
Roelof Roos <github@roelofr.dev>
2025-07-01 16:24:05 +0200

    test: Fix test

--- src/test/java/dev/roelofr/rest/resources/AuthenticationResourceTest.java
+++ src/test/java/dev/roelofr/rest/resources/AuthenticationResourceTest.java
@@ -1,28 +1,13 @@
 package dev.roelofr.rest.resources;
 
-import dev.roelofr.MockDomainHelper;
-import dev.roelofr.config.Roles;
-import dev.roelofr.domain.enums.TicketStatus;
-import dev.roelofr.repository.DistrictRepository;
-import dev.roelofr.repository.TicketRepository;
-import io.quarkus.test.InjectMock;
-import io.quarkus.test.common.http.TestHTTPEndpoint;
-import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.security.TestSecurity;
+import static dev.roelofr.MockDomainHelper.EMAIL_USER;
+import static io.restassured.RestAssured.with;
+
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Stream;
-
-import static io.restassured.RestAssured.when;
-import static io.restassured.RestAssured.with;
-import static io.restassured.RestAssured.when;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.iterableWithSize;
-import static org.mockito.BDDMockito.given;
-import static dev.roelofr.MockDomainHelper.TEST_USER;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 
 @QuarkusTest
 @TestHTTPEndpoint(TicketResource.class)
@@ -42,7 +27,7 @@ class AuthenticationResourceTest {
                   "username": "%s",
                   "password": "testtest"
                 }
-                """.formatted(TEST_USER))
+                """.formatted(EMAIL_USER))
             .post("/login")
             .then()
             .statusCode(200);
