84c70d87f38ed9c47fe86f0248dfb330b7ecf1ff
Roelof Roos <github@roelofr.dev>
2025-07-21 20:39:07 +0200

    fix: fix me response

--- src/main/java/dev/roelofr/rest/responses/AuthMeResponse.java
+++ src/main/java/dev/roelofr/rest/responses/AuthMeResponse.java
@@ -1,22 +1,42 @@
 package dev.roelofr.rest.responses;
 
+import dev.roelofr.domain.District;
 import dev.roelofr.domain.User;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
+import java.util.List;
 import java.util.Set;
 
 public record AuthMeResponse(
     String subject,
     String keyId,
-    User user,
+    EmbeddedUser user,
     Set<String> roles
 ) {
     public AuthMeResponse(JsonWebToken jwt, User user) {
         this(
             jwt.getSubject(),
             jwt.getTokenID(),
-            user,
+            new EmbeddedUser(user),
             jwt.getGroups()
         );
+    }
+
+    record EmbeddedUser(
+        Long id,
+        String name,
+        String email,
+        List<String> roles,
+        District district
+    ) {
+        EmbeddedUser(User user) {
+            this(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getRoles(),
+                user.getDistrict()
+            );
+        }
     }
 }
