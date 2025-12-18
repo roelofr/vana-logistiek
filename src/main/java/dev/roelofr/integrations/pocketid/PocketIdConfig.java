c748bceaafef625a1aa1563ce7d4f4dd12d1e7ed
Roelof Roos <github@roelofr.dev>
2025-12-18 08:35:06 +0100

    fix: pocket id api key

--- src/main/java/dev/roelofr/integrations/pocketid/PocketIdConfig.java
+++ src/main/java/dev/roelofr/integrations/pocketid/PocketIdConfig.java
@@ -1,16 +1,12 @@
 package dev.roelofr.integrations.pocketid;
 
 import io.smallrye.config.ConfigMapping;
-import io.smallrye.config.WithDefault;
 
 @ConfigMapping(prefix = "app.services.pocket-id")
 public interface PocketIdConfig {
-    @WithDefault("false")
     boolean enabled();
 
-    @WithDefault("https://example.com")
     String url();
 
-    @WithDefault("unset")
     String apiKey();
 }
