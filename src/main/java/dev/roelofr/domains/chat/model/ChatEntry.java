62a0e5bd1b07942c4b2295fee40995262f2a3d01
Roelof Roos <github@roelofr.dev>
2026-04-19 20:32:42 +0200

    feat: more chat stuffs

--- src/main/java/dev/roelofr/domains/chat/model/ChatEntry.java
+++ src/main/java/dev/roelofr/domains/chat/model/ChatEntry.java
@@ -1,13 +1,17 @@
 package dev.roelofr.domains.chat.model;
 
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@DiscriminatorColumn(name = "type", length = 16)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public abstract class ChatEntry extends ChatModel {
     @ManyToOne
-    @JoinColumn(name = "chat_id")
+    @JoinColumn(name = "chat_id", nullable = false, updatable = false)
     Chat chat;
 }
-
---
+++ src/main/java/dev/roelofr/domains/chat/model/ChatMessage.java
@@ -0,0 +1,23 @@
+package dev.roelofr.domains.chat.model;
+
+import dev.roelofr.domains.users.Group;
+import dev.roelofr.domains.users.User;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@DiscriminatorValue(value = ChatMessage.TYPE)
+public class ChatMessage extends ChatEntry {
+    public static final String TYPE = "message";
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", updatable = false)
+    public User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "group_id", updatable = false)
+    public Group group;
+
+    public String message;
+}
